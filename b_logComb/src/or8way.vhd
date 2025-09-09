fc17ff1e61dfba60820cd4cd216ce86ed989226b
josephperl <joseph@al.insper.edu.br>
2025-09-09 15:51:10 -0300

    or8way funcionando e testado

--- b_logComb/src/or8way.vhd
+++ b_logComb/src/or8way.vhd
@@ -17,6 +17,8 @@ end entity;
 architecture arch of or8way is
 	signal word: STD_LOGIC_VECTOR(7 downto 0);
 begin
-
-
+    word <= a & b & c & d & e & f & g & h; -- concatena em um vetor
+    q <= word(0) or word(1) or word(2) or word(3) or 
+         word(4) or word(5) or word(6) or word(7);
+		 
 end architecture;
