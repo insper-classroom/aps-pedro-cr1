0f31b4afc93588153aa975e09fe598426290be29
Pedro <pedroaverame@gmail.com>
2025-09-10 20:14:37 -0300

    Conceito A+

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/tgRCPRor)
 
+
+## LINK VIDEO: https://youtube.com/shorts/8-ezzWqeykY?feature=share
+
+
+-   "nome": "Láisa Camilly de Oliveira",
+    "email-insper": "laisaco1@al.insper.edu.br",
+    "user-github": "laisacamilly"
+ 
+-   "nome": "Enrico Castro Guarita",
+    "email-insper": "enricocg@al.insper.edu.br",
+    "user-github": "enricocg21"
+ 
+-   "nome": "Caio Souza Rosa",
+    "email-insper": "Caiosr2@al.insper.edu.br",
+    "user-github": "Caiosr2"
+    
+-   "nome": "Pedro Averame Kramer",
+    "email-insper": "pedroak@al.insper.edu.br",
+    "user-github": "P-Kramer"
+
+-   "nome": "Felipe Serra Silva",
+    "email-insper": "felipess16@al.insper.edu.br",
+    "user-github": "Felipe-Serra-Silva"
+
+-   "nome": "Joseph Perl",
+    "email-insper": "josephp@al.insper.edu.br",
+    "user-github": "josephperl"
+    
+
 ```
 export SIM=ghdl
 pip3 install -r requirements.txt
@@ -23,4 +52,3 @@ Digite no terminal uma única vez para habilitar o uso do simulador ghdl
 
 ```bash
 echo 'export SIM=ghdl' >> $HOME/elementos.sh
-```
--- b_logComb/src/TopLevel.vhd
+++ b_logComb/src/TopLevel.vhd
@@ -24,7 +24,10 @@ entity TopLevel is
 		LEDR     : out std_logic_vector(9 downto 0);
 		HEX0     : out std_logic_vector(6 downto 0);
 		HEX1     : out std_logic_vector(6 downto 0);
-		HEX2     : out std_logic_vector(6 downto 0)
+		HEX2     : out std_logic_vector(6 downto 0);
+		HEX3     : out std_logic_vector(6 downto 0);
+		HEX4     : out std_logic_vector(6 downto 0);
+		HEX5     : out std_logic_vector(6 downto 0)
 	);
 end entity;
 
@@ -33,39 +36,31 @@ end entity;
 ----------------------------
 architecture rtl of TopLevel is
 
-component conceitoa is
-	port(
-	
-		 SW      : in  std_logic_vector(9 downto 0);
-       HEX0    : out std_logic_vector(6 downto 0); -- 7seg0
-       LEDR    : out std_logic_vector(9 downto 0)
-	);
-	
+component sevenseg is
+		port (
+			bcd : in  std_logic_vector(3 downto 0);
+			leds: out std_logic_vector(6 downto 0)
+			);
+			
 end component;
 
 --------------
 -- signals
 --------------
- signal X,Y,Z: std_logic;
- signal expressao: std_logic;
-
+ signal X: std_logic_vector(3 downto 0);
+ 
 
 ---------------
 -- implementacao
 ---------------
 begin
-	X<= SW(0);
-	Y<= SW(1);
-	Z<=SW(2);
-	expressao<= (not X) or (not Y) or (not Z);
-	LEDR <= "1111111111" when expressao = '1' else
-				"0000000000";
-	HEX0<= "0010010";
-	HEX1<= "0010010";
-	HEX2<= "0010010";
-
-	HEX0 <= "0010010";
-		
+	X <= "00" & SW(9 downto 8);
+	u1: sevenseg port map(bcd=> SW(3 downto 0), leds=> HEX0);
+	u2: sevenseg port map(bcd=> SW(7 downto 4),leds =>HEX1);
+	u3: sevenseg port map(bcd=> X,leds =>HEX2);
+	HEX3 <= "1111111";
+	HEX4 <= "1111111";
+	HEX5 <= "1111111";
  
 end rtl;
 
