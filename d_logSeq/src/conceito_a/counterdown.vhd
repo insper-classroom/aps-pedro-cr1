990227ac529d73d9e8a20fd80563a16e6d1e247a
Pedro <pedroaverame@gmail.com>
2025-10-08 16:26:18 -0300

    wave counterdown

--- d_logSeq/src/conceito_a/counterdown.vhd
+++ d_logSeq/src/conceito_a/counterdown.vhd
@@ -20,23 +20,20 @@ architecture arch of CounterDown is
         );
     end component;
 
-    signal q0, q1, q2 : std_logic := '0';
-    signal nq0, nq1   : std_logic;
-    signal t0, t1, t2 : std_logic;
+    signal q0, q1, q2 : std_logic;      -- estados
+    signal nq0, nq1, nq2 : std_logic;   -- notQ de cada estágio
+    signal t0, t1, t2 : std_logic;      -- entradas T
 begin
-    -- Complementos
-    nq0 <= not q0;
-    nq1 <= not q1;
+    -- Entradas T (down-counter)
+    t0 <= '1';               -- Q0 alterna a cada clock
+    t1 <= nq0;               -- Q1 alterna quando Q0 = 0
+    t2 <= nq0 and nq1;       -- Q2 alterna quando Q0=0 e Q1=0
 
-    -- Entradas T
-    t0 <= '1';             -- alterna a cada clock
-    t1 <= nq0;             -- alterna quando Q0 = 0
-    t2 <= nq0 and nq1;     -- alterna quando Q0=0 e Q1=0
-
-    -- Flip-flops
+    -- Flip-flops T (use SEMPRE o notq dos FFs, nada de reatribuir)
     U0: FlipFlopT port map(clock => clock, t => t0, q => q0, notq => nq0);
     U1: FlipFlopT port map(clock => clock, t => t1, q => q1, notq => nq1);
-    U2: FlipFlopT port map(clock => clock, t => t2, q => q2, notq => open);
+    U2: FlipFlopT port map(clock => clock, t => t2, q => q2, notq => nq2);
 
+    -- Saída [2..0]
     q <= q2 & q1 & q0;
 end architecture;
